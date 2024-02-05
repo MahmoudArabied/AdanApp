e7e8416a5d0684c5479f4a0e7c7c37e5f58fc3b1
Mahmoud El-Arabied <m.elarabied@hotmail.com>
2024-02-05 16:16:33 +0100

    First commit to project settings and main folders

---
+++ AdanExpress/AdanUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AdanUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
